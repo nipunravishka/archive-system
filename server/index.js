1853287e9db82f79008bc7c29c942c9010cd73d2
nipunravishka <nipunravishka19@gmail.com>
2026-03-04 14:55:41 +0530

    Update port for hosting

--- server/index.js
+++ server/index.js
@@ -21,5 +21,5 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
